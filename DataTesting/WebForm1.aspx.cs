e56457145e0652a9e7f9f8a9f832fdb82c5cf5b5
Mads Veelsgaard <mazznielsen@hotmail.com>
2017-10-03 13:12:55 +0200

    Lavet connection og commands, men ExecuteNonQuery virker ikke, ellers burde det.

--- DataTesting/WebForm1.aspx.cs
+++ DataTesting/WebForm1.aspx.cs
@@ -6,19 +6,32 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Data;
 
 namespace DataTesting
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+
+        
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-893TL49\OAKENS;Initial Catalog=ThoraDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        
         protected void Page_Load(object sender, EventArgs e)
         {
+            
             
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            //hej
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into dbo.MedarbejderDim('"+TextBox1+"', '"+TextBox2+"')";
+            cmd.ExecuteNonQuery();
+
+            con.Close();
         }
     }
 }
